2b679d0ff43fe72ee67aa8ff92a9141b65aa707a
Mo <mshamsi2@illinois.edu>
2018-02-06 14:38:36 -0600

    Initial commit

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,42 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class WebScraper {
     public static void main(String[] unused) {
-        System.out.print("ran");
+        System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
+        int wordCount = urlToString("http://erdani.com/tdpl/hamlet.txt").length();
+        System.out.println("word count is " + wordCount);
+        String urlString = urlToString("http://erdani.com/tdpl/hamlet.txt").toUpperCase();
+        String[] theSplitString = urlString.split(" ");
+        int countAppearance = 0;
+        for (int i = 0; i < theSplitString.length - 1; i++) {
+            if (theSplitString[i].contains("PRINCE")) {
+                countAppearance++;
+            }
+        }
+        System.out.println("count appearance is " + countAppearance);
     }
+
+
+
+/**
+        * Retrieve contents from a URL and return them as a string.
+        *
+        * @param url url to retrieve contents from
+        * @return the contents from the url as a string, or an empty string on error
+        */
+        public static String urlToString(final String url) {
+            Scanner urlScanner;
+            try {
+                urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+                } catch (IOException e) {
+                return "";
+                }
+                String contents = urlScanner.useDelimiter("\\A").next();
+                urlScanner.close();
+                return contents;
+
+        }
 }
